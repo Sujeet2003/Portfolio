05906f29b7a9e5c227d1b90d4174947d29b7e579
Sujeet <sujeetsmpatel@gmail.com>
2023-10-29 12:12:54 +0530

    first commit

---
+++ Portfolio/views.py
@@ -0,0 +1,43 @@
+from django.shortcuts import render
+
+def home(request):
+    name = {
+        'title': 'Home_Page',
+    }
+    return render(request, 'home.html', name)
+def skills(request):
+    name = {
+        'title': 'Skills_Page',
+    }
+    return render(request, 'skills.html', name)
+def projects(request):
+    name = {
+        'title': 'Project_Page',
+    }
+    return render(request, 'project.html', name)
+def academics(request):
+    name = {
+        'title': 'Academics_Page',
+    }
+    return render(request, 'academic.html', name)
+def contact(request):
+    values = {}
+    dict_values = {
+        'title': 'Contact_Page',
+        'data': values,
+    }
+    try:
+        if request.method == 'POST':
+            name = request.GET['name']
+            phone = request.GET['phone']
+            email = request.GET['email']
+            description = request.GET['description']
+            values = {
+                'name': name,
+                'phone': phone,
+                'email': email,
+                'description': description,
+            }
+    except:
+        pass
+    return render(request, 'contact.html', dict_values)
